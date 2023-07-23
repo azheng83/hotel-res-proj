e91816028ba588f07feb3320ede3a0522ca92cd6
Daniel <daniel82277@gmail.com>
2023-07-22 20:40:05 -0400

    booking update

--- hhr_2023/src/app/booking/booking.component.ts
+++ hhr_2023/src/app/booking/booking.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./booking.component.css']
 })
 export class BookingComponent {
-
+  bookNextStay: string = 'Book Your Next Stay'; 
+  name: string = 'Name:'; 
+  email: string = 'Email:'; 
+  telNumber: string = 'Phone Number:';
+  checkIn: string = 'Check-In:';
+  checkOut: string = 'Check-Out'; 
+  numOfGuests: string = 'Number of Guests:'; 
+  roomType: string = 'Room Type:'; 
 }
