4ca7873607350f58ac59c3cec0916bf74a17e1d6
Daniel <daniel82277@gmail.com>
2023-07-23 20:28:52 -0400

    router update and others

--- hhr_2023/src/app/app.component.ts
+++ hhr_2023/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { BookingComponent } from './booking/booking.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- hhr_2023/src/app/app.module.ts
+++ hhr_2023/src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    PageNotFoundComponent,
+    RoomsComponent,
+    AboutUsComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'home', component: HomeComponent}, 
+      {path: 'rooms', component: RoomsComponent}, 
+      {path: 'aboutus', component: AboutUsComponent},
+      {path: 'contact', component: ContactComponent}, 
+      {path: '**', component: PageNotFoundComponent}, 
+      {path: '', redirectTo: '/home', pathMatch: 'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
