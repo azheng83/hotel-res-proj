cab760a2154583da96a6be656bf02903da535fa5
azheng83 <andriazheng1@gmail.com>
2023-07-23 21:10:10 -0400

    css replacement

--- hhr_2023/src/app/booking/booking.component.ts
+++ hhr_2023/src/app/booking/booking.component.ts
@@ -10,8 +10,8 @@ export class BookingComponent {
   name: string = 'Name:'; 
   email: string = 'Email:'; 
   telNumber: string = 'Phone Number:';
-  checkIn: string = 'Check-In:';
-  checkOut: string = 'Check-Out'; 
-  numOfGuests: string = 'Number of Guests:'; 
+  checkIn: string = 'Check-In:';  //make this an interface instead, import interface
+  checkOut: string = 'Check-Out'; //for the interface, make them data types for the input, then choose a label for them. 
+  numOfGuests: string = 'Number of Guests:'; //in html file. In TS file: use django rest framework API to capture data & send to the database, method creation
   roomType: string = 'Room Type:'; 
 }
